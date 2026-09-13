b9a133e58a76cd163b19d6f22af7b11dfee4c4e2
Karya Anak Bangsa <tkab.alamsyah@gmail.com>
2026-09-14 01:09:08 +0700

    Persiapan untuk create dan edit di modul kerjasama.

--- resources/views/mod_kerjasama/create.blade.php
+++ resources/views/mod_kerjasama/create.blade.php
@@ -10,7 +10,7 @@
 @section('page-content')
 
     <x-page-body-form title="Form Tambah Kerjasama">
-        <form action="{{ route('kerjasama.store') }}" method="post" enctype="multipart/form-data">
+        <form action="{{-- ke controller store --}}" method="post" enctype="multipart/form-data">
             @csrf
             @include('mod_kerjasama._form')
         </form>
