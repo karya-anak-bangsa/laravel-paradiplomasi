c248b9cef4e99a7480da8cff4a6613b0d5dd9766
Karya Anak Bangsa <tkab.alamsyah@gmail.com>
2026-09-17 17:24:18 +0700

    Perbaikan tampilan show kedutaan besar untuk card rincian data.

--- resources/views/components/show-field.blade.php
+++ resources/views/components/show-field.blade.php
@@ -1,13 +1,4 @@
-<div class="datagrid align-items-center mb-5">
-    <div class="datagrid-item">
-        <div class="datagrid-title">Format Undangan</div>
-        <div class="datagrid-content">{{ $kedutaanBesar->format_undangan }}</div>
-    </div>
-    <div class="datagrid-item">
-        <div class="datagrid-title">Bendera Negara</div>
-        <div class="datagrid-content d-flex align-items-center">
-            <span class="flag flag-md flag-country-{{ $kedutaanBesar->kode_negara }} me-2"></span>
-            <span class="fw-bold">{{ $kedutaanBesar->nama_negara }}</span>
-        </div>
-    </div>
+<div class="datagrid-item">
+    <div class="datagrid-title">{{ $label }}</div>
+    <div class="datagrid-content">{{ $slot }}</div>
 </div>
--- resources/views/mod_kedutaan_besar/show-rincian.blade.php
+++ resources/views/mod_kedutaan_besar/show-rincian.blade.php
@@ -1,40 +1,23 @@
-{{-- <div class="hr-text hr-text-start">Diplomasi</div> --}}
 <div class="datagrid align-items-center mb-5">
-    <div class="datagrid-item">
-        <div class="datagrid-title">Format Undangan</div>
-        <div class="datagrid-content">{{ $kedutaanBesar->format_undangan }}</div>
-    </div>
-    <div class="datagrid-item">
-        <div class="datagrid-title">Bendera Negara</div>
-        <div class="datagrid-content d-flex align-items-center">
+    <x-show-field label="Format Undangan">{{ $kedutaanBesar->format_undangan }}</x-show-field>
+    <x-show-field label="Bendera Negara">
+        <div class="d-flex align-items-center">
             <span class="flag flag-md flag-country-{{ $kedutaanBesar->kode_negara }} me-2"></span>
             <span class="fw-bold">{{ $kedutaanBesar->nama_negara }}</span>
         </div>
-    </div>
+    </x-show-field>
 </div>
 <div class="datagrid align-items-center mb-5">
-    <div class="datagrid-item">
-        <div class="datagrid-title">Kedutaan Besar</div>
-        <div class="datagrid-content">
-            <p class="fst-normal mb-0">{{ $kedutaanBesar->nama_kedutaan_besar_id ?? '-' }}</p>
-            <small class="fst-italic text-primary mb-0">{{ $kedutaanBesar->nama_kedutaan_besar_en ?? '-' }}</small>
-        </div>
-    </div>
-    <div class="datagrid-item">
-        <div class="datagrid-title">Diplomat</div>
-        <div class="datagrid-content">
-            <p class="fst-normal mb-0">{{ $kedutaanBesar->nama_diplomat ?? '-' }}</p>
-            <small class="fst-italic text-primary mb-0">{{ $kedutaanBesar->jabatan_diplomat ?? '-' }}</small>
-        </div>
-    </div>
+    <x-show-field label="Kedutaan Besar">
+        <p class="fst-normal mb-auto">{{ $kedutaanBesar->nama_kedutaan_besar_id ?? '-' }}</p>
+        <small class="fst-italic text-primary mb-auto">{{ $kedutaanBesar->nama_kedutaan_besar_en ?? '-' }}</small>
+    </x-show-field>
+    <x-show-field label="Diplomat">
+        <p class="fst-normal mb-auto">{{ $kedutaanBesar->nama_diplomat ?? '-' }}</p>
+        <small class="fst-italic text-primary mb-auto">{{ $kedutaanBesar->jabatan_diplomat ?? '-' }}</small>
+    </x-show-field>
 </div>
 <div class="datagrid align-items-center mb-0">
-    <div class="datagrid-item">
-        <div class="datagrid-title">Email</div>
-        <div class="datagrid-content">{{ $kedutaanBesar->email_kantor ?? '-' }}</div>
-    </div>
-    <div class="datagrid-item">
-        <div class="datagrid-title">Telepon</div>
-        <div class="datagrid-content">{{ $kedutaanBesar->telepon_kantor ?? '-' }}</div>
-    </div>
+    <x-show-field label="Email">{{ $kedutaanBesar->email_kantor ?? '-' }}</x-show-field>
+    <x-show-field label="Telepon">{{ $kedutaanBesar->telepon_kantor ?? '-' }}</x-show-field>
 </div>
