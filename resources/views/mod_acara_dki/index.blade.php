a805860b365b04d8f1be081be207c69e4eb310e6
Karya Anak Bangsa <tkab.alamsyah@gmail.com>
2026-09-23 05:16:58 +0700

    Perbaikan index.blade.php di modul acara dki.

--- resources/views/mod_acara_dki/index.blade.php
+++ resources/views/mod_acara_dki/index.blade.php
@@ -15,23 +15,22 @@
     <x-page-body-table title="Daftar acara DKI">
         <x-slot name="thead">
             <tr>
-                <th data-orderable="true" style="width:18%" class="text-start">Acara DKI</th>
-                <th data-orderable="true" style="width:15%" class="text-start">Pelaksana</th>
-                <th data-orderable="true" style="width:14%" class="text-center">Mitra</th>
-                <th data-orderable="true" style="width:13%" class="text-center">Tanggal Diterima</th>
-                <th data-orderable="true" style="width:13%" class="text-center">Tanggal Selesai</th>
-                <th data-orderable="true" style="width:12%" class="text-center">Status</th>
-                <th data-orderable="true" style="width:15%" class="text-center">Aksi</th>
+                <th data-orderable="true" style="width:22%" class="text-start">Acara DKI</th>
+                <th data-orderable="true" style="width:16%" class="text-center">Daftar Undangan</th>
+                <th data-orderable="true" style="width:15%" class="text-center">Tanggal Diterima</th>
+                <th data-orderable="true" style="width:15%" class="text-center">Tanggal Selesai</th>
+                <th data-orderable="true" style="width:14%" class="text-center">Status</th>
+                <th data-orderable="true" style="width:18%" class="text-center">Aksi</th>
             </tr>
         </x-slot>
         <x-slot name="tbody">
             @foreach ($acaraDki as $item)
                 <tr>
                     <td class="text-start">{{ $item->judul_ringkas }}</td>
-                    <td class="text-start">{{ $item->pelaksana ?? '-' }}</td>
                     <td class="text-center">
-                        <span class="badge bg-blue-lt">{{ $item->mitra->count() }} Diundang</span>
-                        <span class="badge bg-success-lt">{{ $item->mitra->where('pivot.status_kehadiran', 'Hadir')->count() }} Hadir</span>
+                        <span class="badge bg-blue-lt d-block mb-1">{{ $item->mitra->where('pivot.status_kehadiran', 'Diundang')->count() }} Diundang</span>
+                        <span class="badge bg-success-lt d-block mb-1">{{ $item->mitra->where('pivot.status_kehadiran', 'Hadir')->count() }} Hadir</span>
+                        <span class="badge bg-danger-lt d-block">{{ $item->mitra->where('pivot.status_kehadiran', 'Tidak Hadir')->count() }} Tidak Hadir</span>
                     </td>
                     <td class="text-center">{{ $item->tanggal_diterima_display }}</td>
                     <td class="text-center">{!! $item->tanggal_selesai_display !!}</td>
